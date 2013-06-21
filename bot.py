5e0e4dc637b07b23430bb2bd894502dec8be0bed
Kunal Mehta <legoktm@gmail.com>
2013-06-20 22:25:03 -0700

    create the API

--- bot.py
+++ bot.py
@@ -28,6 +28,7 @@ import tweepy
 import config
 auth = tweepy.OAuthHandler(config.consumer_key, config.consumer_secret)
 auth.set_access_token(config.access_token, config.access_token_secret)
+api = tweepy.API(auth)
 
 
 def run(**kw):
